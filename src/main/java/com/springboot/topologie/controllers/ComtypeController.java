74cc1a609183ea90adcc40cfb3c8cf48535d295e
Tanja Heinrich <ta391hei@htwg-konstanz.de>
2021-06-15 18:01:32 +0200

    Added missing files for Comtype

--- src/main/java/com/springboot/topologie/controllers/ComtypeController.java
+++ src/main/java/com/springboot/topologie/controllers/ComtypeController.java
@@ -1,4 +1,54 @@
 package com.springboot.topologie.controllers;
 
+import com.springboot.topologie.models.Channel;
+import com.springboot.topologie.models.Comtype;
+import com.springboot.topologie.models.data.ChannelDAO;
+import com.springboot.topologie.models.data.ComTypeDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping (value = "comtype")
 public class ComtypeController {
+
+    @Autowired
+    ComTypeDAO comtypeDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("title", "Comtype");  //passes titel "comtype" to tag ${title} in channel index.html file.
+        model.addAttribute("comtypes", comtypeDao.findAll());
+        return "comtype/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddChannelForm(Model model) {
+        model.addAttribute("title", "Add Comtype");
+        model.addAttribute(new Comtype());
+        return "comtype/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Comtype comtype, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Comtype");
+            return "comtype/add";
+        }
+        comtypeDao.save(comtype);
+        return "redirect:view/" + comtype.getId();
+    }
+
+    @RequestMapping(value = "view/{comtypeId}", method = RequestMethod.GET)
+    public String viewComtype(Model model, @PathVariable int comtypeId) {
+        Comtype comtype = comtypeDao.findById(comtypeId).orElse(null);
+        model.addAttribute("title", comtype.getName());
+        model.addAttribute("comtypeId", comtype.getId());
+        return "comtype/view";
+    }
 }
--- src/main/java/com/springboot/topologie/models/data/ComTypeDAO.java
+++ src/main/java/com/springboot/topologie/models/data/ComTypeDAO.java
@@ -1,4 +1,11 @@
 package com.springboot.topologie.models.data;
 
-public interface ComTypeDAO {
-}
+import com.springboot.topologie.models.Communication;
+import com.springboot.topologie.models.Comtype;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface ComTypeDAO extends CrudRepository<Comtype, Integer> {}
